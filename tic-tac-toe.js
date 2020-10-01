c241b8d2b6d9930841b6f3d60f1b26d03b4cc0e3
Elyse Steingold <elyselindsays@gmail.com>
2020-10-01 19:10:13 -0400

    Gameplay logic trials

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,15 +3,22 @@ window.addEventListener("DOMContentLoaded", event => {
     //let squares = document.querySelectorAll(".square");
     let turnCount = 0;
     board.addEventListener("click", event => {
+        console.log(event.target.id);
         let token = document.createElement("img");
-            if (turnCount % 2 === 0) {
-                token.src = "images/player-x.svg";
-                event.target.appendChild(token);
-                turnCount++;
-            } else {
-                token.src = "images/player-o.svg";
-                event.target.appendChild(token);
-                turnCount++;
-            }
+        if (turnCount % 2 === 0) {
+            token.src = "images/player-x.svg";
+            console.log(event.target.value);
+            event.target.appendChild(token);
+            turnCount++;
+
+        } else {
+            token.src = "images/player-o.svg";
+            console.log(event.target.value);
+            event.target.appendChild(token);
+            turnCount++;
+
+        }
+
+
     });
 });
