d34ae6805b24b36ad076dda7b132b943732b27e5
Elyse Steingold <elyselindsays@gmail.com>
2020-10-01 18:35:49 -0400

    Click event listener + X's

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,33 @@
-consdofnajgnf
+window.addEventListener("DOMContentLoaded", event => {
+    let board = document.getElementById("tic-tac-toe-board");
+    let squares = document.querySelectorAll(".square");
+    console.log(squares);
+    board.addEventListener("click", event => {
+        let token = document.createElement("img");
+
+        for (let i = 0; i < squares.length; i++) {
+            let turnCount = 0;
+
+            if (i % 2 === 0) {
+                token.src = "images/player-x.svg";
+                event.target.appendChild(token);
+            } else {
+                token.src = "images/player-o.svg";
+                event.target.appendChild(token);
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
