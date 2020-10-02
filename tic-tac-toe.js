89d5029774715a10ce61977412d2df19da1c06aa
Rihana Chang <martinson.r@gmail.com>
2020-10-02 14:11:30 -0700

    Added tile matching and winner declaration functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,33 @@ window.addEventListener("DOMContentLoaded", event => {
     let board = document.getElementById("tic-tac-toe-board");
     let squareValues = ["", "", "", "", "", "", "", "", ""];
     let currentPlayerSymbol = "x";
+    let gameStatus = "";
+    let announcement = document.getElementById("game-status");
+
+
+    let gameLoop = function() {
+        for (i = 0; i < 9; i+=3) {
+            if ((squareValues[i] !== "") && (squareValues[i] === squareValues[i+1]) && (squareValues[i+1] === squareValues[i+2])) {
+                gameStatus = squareValues[i];
+                console.log(gameStatus);
+            }
+        }
+
+        for (i = 0; i < 3; i+=1) {
+            if ((squareValues[i] !== "") && (squareValues[i] === squareValues[i+3]) && (squareValues[i+3] === squareValues[i+6])) {
+                gameStatus = squareValues[i];
+            }
+
+            if ((squareValues[0] !== "") && (squareValues[0] === squareValues[4]) && (squareValues[0] === squareValues[8])) {
+                gameStatus = squareValues[0];
+            }
+
+            if ((squareValues[2] !== "") && (squareValues[2] === squareValues[4]) && (squareValues[2] === squareValues[6])) {
+                gameStatus = squareValues[2];
+            }
+    }
+}
+
     board.addEventListener("click", event => {
         if (event.target.id.includes("square")) {
             let thisTarget = event.target.id;
@@ -24,6 +51,9 @@ window.addEventListener("DOMContentLoaded", event => {
         } else {
             console.log("Nope!");
         }
-        console.log(squareValues);
+        gameLoop();
+        if (gameStatus !== "") {
+            announcement.innerHTML = "The winner is " + gameStatus;
+        }
     });
 });
