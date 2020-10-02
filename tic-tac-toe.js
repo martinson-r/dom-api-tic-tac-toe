4a2cf7e77d3b7cf5de3bb8829162b3389f47a970
Rihana Chang <martinson.r@gmail.com>
2020-10-02 10:34:50 -0700

    Reworked game logic

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,47 +1,29 @@
 window.addEventListener("DOMContentLoaded", event => {
     let board = document.getElementById("tic-tac-toe-board");
-    //let squares = document.querySelectorAll(".square");
-    let turnCount = 0;
-    let player1 = [];
-    let player2 = [];
+    let squareValues = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayerSymbol = "x";
     board.addEventListener("click", event => {
-        console.log(event.target.className);
-        let token = document.createElement("img");
-        if (turnCount % 2 === 0) {
-
-            if (event.target.className === "filled") {
-                return false;
-            }
-
-            token.src = "images/player-x.svg";
-            token.className = "filled";
-            event.target.appendChild(token);
-            turnCount++;
-            player1.push(event.target.className);
-            let player1Score = 0;
-            for (i = 0; i < player1.length; i++) {
-                if (player1[i].includes(event.target.className)) {
-                    player1Score++;
+        if (event.target.id.includes("square")) {
+            let thisTarget = event.target.id;
+            let number = thisTarget.slice(7);
+            if (squareValues[number] === "") {
+                let token = document.createElement("img");
+                token.src = "images/player-x.svg";
+                event.target.appendChild(token);
+                squareValues[number] = currentPlayerSymbol;
+                if (currentPlayerSymbol === "x") {
+                    currentPlayerSymbol = "o";
+                } else {
+                    currentPlayerSymbol = "x";
+                    token.src = "images/player-o.svg";
                 }
+            } else {
+                console.log("Nope!");
             }
 
         } else {
-
-            if (event.target.className === "filled") {
-                return false;
-            }
-            
-            player2.push(event.target.className);
-            let player2Score = 0;
-            for (j = 0; j < player2.length; j++) {
-                if (player2[j].includes(event.target.className)) {
-                    player2Score++;
-                }
-            }
-            token.src = "images/player-o.svg";
-            token.className = "filled";
-            event.target.appendChild(token);
-            turnCount++;
+            console.log("Nope!");
         }
+        console.log(squareValues);
     });
 });
